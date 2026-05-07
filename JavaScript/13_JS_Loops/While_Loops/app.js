53056e69cd76edb2e12146e82af8b531470cd5c1
Gustavo Avelino Sanchez <160865518+gusanchez1@users.noreply.github.com>
2026-05-07 17:18:14 -0300

    guessing game bugs fixes

--- JavaScript/13_JS_Loops/While_Loops/app.js
+++ JavaScript/13_JS_Loops/While_Loops/app.js
@@ -1,29 +1,3 @@
-// let count = 0;
-// while (count < 10) {
-//     count++;
-//     console.log(count)
-// }
-
-// const SECRET = "BabyHippo";
-
-// let guess = prompt("enter the secret code...");
-// while (guess !== SECRET) {
-//     guess = prompt("enter the secret code...");
-// }
-// console.log("CONGRATS YOU GOT THE SECRET!!!")
-
-// let input = prompt("Hey, say something!")
-// while (true) {
-//     input = prompt(input);
-//     if (input.toLowerCase() === "stop copying me") break;
-// }
-// console.log("OK YOU WIN!")
-
-// for (let i = 0; i < 1000; i++) {
-//     console.log(i);
-//     if (i === 100) break;
-// }
-
 let maximum = parseInt(prompt("Enter the maximum number!"));
 while (!maximum) {
     maximum = parseInt(prompt("Enter a valid number!"));
@@ -31,16 +5,20 @@ while (!maximum) {
 
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 
-let guess = parseInt(prompt("Enter your first guess!"));
+let guess = prompt("Enter your first guess! (Type 'q' to quit)");
 let attempts = 1;
 
 while (parseInt(guess) !== targetNum) {
     if (guess === 'q') break;
-    attempts++;
+    guess = parseInt(guess);
     if (guess > targetNum) {
         guess = prompt("Too high! Enter a new guess:");
-    } else {
+        attempts++;
+    } else if (guess < targetNum) {
         guess = prompt("Too low! Enter a new guess:");
+        attempts++;
+    } else {
+        guess = prompt("Invalid guess. Please enter a number or 'q' to quit");
     }
 }
 
